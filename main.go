5a40b4214e36a2dc88802eabdb9a8eb06cd082a7
nick <nicksterp123@gmail.com>
2023-12-17 12:50:31 -0800

    init commit for music api

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+
+}
